42079ae8f10cc34b984f7843cc238f1fe06ecb1c
Fionacomcom <34851566+Fionacomcom@users.noreply.github.com>
2019-11-12 11:46:02 +0800

    small change

--- src/test/FlyweightTest.java
+++ src/test/FlyweightTest.java
@@ -21,11 +21,11 @@ public class FlyweightTest {
 		BasePlantProfile pro1= PlantProfileManager.getInstance().getPlantProfile("Cotton");
 		assert (pro1!=null):"测试来自:"+packName+":无法通过反射获取Profile";
 
-		PlantProfileManager.getInstance().addPlantProfile("豌豆射手",100,100,new ProductDropList());
-		BasePlantProfile pro2=PlantProfileManager.getInstance().getPlantProfile("豌豆射手");
+		PlantProfileManager.getInstance().addPlantProfile("Mellon",100,100,new ProductDropList());
+		BasePlantProfile pro2=PlantProfileManager.getInstance().getPlantProfile("Carrot");
 		assert (pro2!=null):"测试来自:"+packName+":无法获取动态建立的Profile";
 
-		BasePlantProfile pro3=PlantProfileManager.getInstance().getPlantProfile("豌豆射手");
+		BasePlantProfile pro3=PlantProfileManager.getInstance().getPlantProfile("Apple");
 		assert (pro3==pro2):"测试来自:"+packName+":获取的不是同一个实例";
 
 		System.out.println("========== Successfully test flyweight! ===========");
